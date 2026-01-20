6bfc053212f67886748da0b9e83f3ec05f28402b
Nursultan <nursutanmusa007@gmail.com>
2026-01-20 19:42:38 +0600

    655

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-
-import './App.css'
 import Home from './components/Home'
-function App() {
 
-  return (
-    <>
-      <Home />
-    </>
-  )
+function App() {
+  return <Home />
 }
 
-export default App;
+export default App

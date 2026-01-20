8be508a3eb5549da37e39608adc8d6ddccf51b3a
Nursultan <nursutanmusa007@gmail.com>
2026-01-20 19:52:47 +0600

    541

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Kyrgyztan/',
+  base: '/Kyrgyztan/',   // 🔴 ЭТО БЫЛО ГЛАВНОЕ
   plugins: [react()],
 })
